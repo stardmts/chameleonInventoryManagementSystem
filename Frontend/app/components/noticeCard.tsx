cd5771cfa399169b918d74fefced9e95ede3efe2
Elliot Hardy <elliothardywork@gmail.com>
2026-06-29 13:10:46 +0100

    notice card finished

--- Frontend/app/components/noticeCard.tsx
+++ Frontend/app/components/noticeCard.tsx
@@ -7,8 +7,11 @@ interface notice {
 
 export default function NoticeCard({noticeId, noticeBody, sender, noticeDate}:notice) {
     return (
-        <main className = "flex flex-row bg-[#323232] items-center space-x-5 w-400 justify-between rounded px-10">
-
+        <main className = "flex flex-row bg-[#323232] items-center space-x-5 w-400 justify-between rounded px-10 py-5 text-white text-2xl">
+            <h2> Notice {noticeId}: </h2>
+            <p> {noticeBody} </p>
+            <p> {sender} </p>
+            <p> {noticeDate} </p>
         </main>
     );
 }
