60e72b9f0784bf0a9abd9c9f1dcf384aad0f079d
Elliot Hardy <elliothardywork@gmail.com>
2026-07-14 17:03:18 +0100

    Backend boilerplate

---
+++ Backend/chameleonims/src/main/java/com/starlight/chameleonims/ChameleonimsApplication.java
@@ -0,0 +1,13 @@
+package com.starlight.chameleonims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChameleonimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChameleonimsApplication.class, args);
+	}
+
+}
---
+++ Backend/chameleonims/src/test/java/com/starlight/chameleonims/ChameleonimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starlight.chameleonims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChameleonimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
